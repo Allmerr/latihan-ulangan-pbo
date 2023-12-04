df175d35b482ac63bc3f370cb8d6b31580f5fc09
Kevin Almer <kevinalmer4@gmail.com>
2023-12-05 05:41:16 +0700

    prepare

--- src/App.js
+++ src/App.js
@@ -2,15 +2,19 @@ import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import NotFound from "./pages/NotFound";
 import About from "./pages/About";
+import PNavbar from "./partials/PNavbar";
+import PFooter from "./partials/PFooter";
 
 function App() {
     return (
         <>
+            <PNavbar></PNavbar>
             <Routes>
                 <Route path="/" element={<Home />}></Route>
                 <Route path="/about" element={<About />}></Route>
                 <Route path="*" element={<NotFound />}></Route>
             </Routes>
+            <PFooter></PFooter>
         </>
     )
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,22 @@
-import Main from "../layouts/Main";
-
-const HomeContent = (
-    <>
-        <h1>Home</h1>
-    </>
-)
+import { Container, Row, Col } from "react-bootstrap";
+import me from "../img/me.png"
+import "../style/home.css"
 
 function Home() {
-    return <Main Content={<HomeContent />} />
+    return (
+        <>
+            <Container>
+                <Row>
+                    <Col md={4}>
+                        <img src={me} alt="" className="w-100" />
+                    </Col>
+                    <Col md={8} className="d-flex align-items-center justify-content-center">
+                        <h1 className="underline">Where Things Become Amazing</h1>
+                    </Col>
+                </Row>
+            </Container>
+        </>
+    )
 }
 
 export default Home;
